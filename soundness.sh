5b20d1b5305106e54a0110c54c39df413036ec91
voyager-1 <cyhzero@gmail.com>
2025-03-27 18:57:02 +0800

    fix: add -y to command and adjust the command order

--- soundness.sh
+++ soundness.sh
@@ -6,16 +6,17 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # 加载 cargo 环境
 . "$HOME/.cargo/env"
 
+# 安装必要的软件包
+sudo apt update
+sudo apt install build-essential -y
+sudo apt install libssl-dev -y
+sudo apt install pkg-config -y
+
 # 安装 soundness-layer
 curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash
 
 # 加载 bashrc
 source ~/.bashrc
-
-# 安装必要的软件包
-apt install build-essential
-apt install libssl-dev
-sudo apt install pkg-config
 
 # 安装并更新 soundnessup CLI
 soundnessup install
@@ -29,3 +30,4 @@ read key_name
 soundness-cli generate-key --name "$key_name"
 
 echo "密钥生成完成。请按照后续提示输入密码。"
+echo "!access <public-key> 发到 DC"
